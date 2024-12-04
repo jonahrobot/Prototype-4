4f9aae6997a468728653f3a4df10e2e82045bee4
Ben Hess <pilotben@comcast.net>
2024-12-04 13:48:31 -0800

    Improved attachment system

--- Assets/Attachable.cs
+++ Assets/Attachable.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Attachable : MonoBehaviour
 {
     public UnityAction stepAction;
--- Assets/AttachmentSystem.cs
+++ Assets/AttachmentSystem.cs
@@ -43,13 +43,16 @@ public class AttachmentSystem : MonoBehaviour
     {
         if (index >= 0 && index < attachments.Length)
         {
+            Rigidbody rb = attachments[index].GetComponent<Rigidbody>();
+            rb.linearVelocity = attachments[index].transform.rotation * new Vector3(0, 5, 0);
+            rb.angularVelocity = new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f);
             attachments[index] = null;
         }
     }
 
     void Attach(int index, Attachable attachable)
     {
-        if (index >= 0 && index < attachments.Length)
+        if (index >= 0 && index < attachments.Length && !attachments.Contains(attachable))
         {
             attachments[index] = attachable;
         }
@@ -71,24 +74,24 @@ public class AttachmentSystem : MonoBehaviour
             {
                 attachments[i].transform.position = attachmentPoints[i].transform.position;
                 attachments[i].transform.rotation = attachmentPoints[i].transform.rotation;
-            }
-        }
-
-        Attachable[] attachables = FindObjectsByType<Attachable>(FindObjectsSortMode.None);
-        foreach(Attachable attachable in attachables)
-        {
-            if(!attachments.Contains(attachable))
-            {
-                if((attachable.transform.position - transform.position).sqrMagnitude < 4)
-                {
-                    Attach(GetNextAttachmentPoint(false), attachable);
-                }
+                Rigidbody rb = attachments[i].GetComponent<Rigidbody>();
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
             }
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Dettach(GetNextAttachmentPoint(true));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Attachable attachable = other.gameObject.GetComponent<Attachable>();
+        if (attachable != null)
+        {
+            Attach(GetNextAttachmentPoint(false), attachable);
         }
     }
 }
