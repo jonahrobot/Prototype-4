970897c49f220d6f1751deb567bafe97b1ed0759
Ben Hess <pilotben@comcast.net>
2024-12-03 16:43:45 -0800

    Added step function

--- Assets/Attachable.cs
+++ Assets/Attachable.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Attachable : MonoBehaviour
 {
+    public UnityAction stepAction;
+
     void Start()
-    {
-        
+    {   
     }
 
     void Update()
     {
-        
     }
 }
